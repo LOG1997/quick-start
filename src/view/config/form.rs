572c9cb1b552b29d89a7c0f548f6a6bb37514915
log1997 <2694233102@qq.com>
2026-05-12 11:32:30 +0800

    feat: new

--- src/view/config/form.rs
+++ src/view/config/form.rs
@@ -16,6 +16,12 @@ pub struct FomrComponent {
     pub type_select_field: Entity<SelectComponent>,
 }
 
+pub struct FormValue {
+    pub name: SharedString,
+    pub value: SharedString,
+    pub type_: SharedString,
+}
+
 impl FomrComponent {
     pub fn new(_window: &mut Window, _cx: &mut Context<Self>) -> Self {
         let name_field = _cx.new(|cx| InputComponent::new(_window, cx, "enter name"));
@@ -29,20 +35,41 @@ impl FomrComponent {
     }
 
     pub fn render_form(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> Form {
-        let name_field = self.name_field.update(_cx, |example, _| {
-            Input::new(&example.input_state) // 正确传递两个参数
+        let name_field = self.name_field.update(_cx, |field, _| {
+            Input::new(&field.input_state) // 正确传递两个参数
         });
-        let value_field = self.value_field.update(_cx, |example, _| {
-            Input::new(&example.input_state) // 正确传递两个参数
+        let value_field = self.value_field.update(_cx, |field, _| {
+            Input::new(&field.input_state) // 正确传递两个参数
         });
         let type_select_field = self
             .type_select_field
-            .update(_cx, |example, _| Select::new(&example.select_state));
+            .update(_cx, |field, _| Select::new(&field.select_state));
 
         let form_block = v_form()
             .child(field().label("Name").child(name_field))
             .child(field().label("Value").child(value_field))
             .child(field().label("Type").child(type_select_field));
         form_block
+    }
+
+    pub fn get_name(&self, _cx: &mut Context<Self>) -> SharedString {
+        self.name_field.update(_cx, |field, _| field.value.clone())
+    }
+
+    pub fn get_value(&self, _cx: &mut Context<Self>) -> SharedString {
+        self.value_field.update(_cx, |field, _| field.value.clone())
+    }
+
+    pub fn get_type(&self, _cx: &mut Context<Self>) -> SharedString {
+        self.type_select_field
+            .update(_cx, |field, _| field.value.clone())
+    }
+
+    pub fn get_form_value(&self, _cx: &mut Context<Self>) -> FormValue {
+        FormValue {
+            name: self.get_name(_cx),
+            value: self.get_value(_cx),
+            type_: self.get_type(_cx),
+        }
     }
 }
--- src/view/view.rs
+++ src/view/view.rs
@@ -18,41 +18,17 @@ impl AppRoot {
         let form = _cx.new(|cx| FomrComponent::new(_window, cx));
         Self { form }
     }
-
-    // pub fn render_form(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> Input {
-    //     self.form.update(_cx, |example, _| {
-    //         Input::new(&example.input_state) // 正确传递两个参数
-    //     })
-    // }
 }
 
 impl Render for AppRoot {
     fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
-        let name_input = _cx.new(|cx| InputState::new(_window, cx).placeholder("Enter your name"));
-        let email_input =
-            _cx.new(|cx| InputState::new(_window, cx).placeholder("Enter your email"));
-        // let helloworld = v_form()
-        //     .child(
-        //         field()
-        //             .label("Run")
-        //             .child(self.render_input(_window, _cx))
-        //             .child(self.input.read(_cx).value.clone()),
-        //     )
-        //     .child(field().label("Name").child(Input::new(&name_input)))
-        //     .child(
-        //         field()
-        //             .label("Email")
-        //             .child(Input::new(&email_input))
-        //             .required(true),
-        //     );
         let form_wrapper = self
             .form
             .update(_cx, |form_ex, cx| form_ex.render_form(_window, cx));
         div()
             .size_full()
             .v_flex()
-            .child(app_header()) // 组件1
-            // .child(helloworld) // 组件2
+            .child(app_header())
             .child(form_wrapper)
     }
 }
